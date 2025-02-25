1967afe587db283d40c53877c3c51044b62f1412
itsnathyall <nathan.caluwaerts@gmail.com>
2025-02-25 13:30:11 +0100

    added base get route

--- src/server.js
+++ src/server.js
@@ -12,5 +12,9 @@ app.use(express.json());
 
 app.use("/", taskRoutes);
 
+app.get('/', (req, res) => {
+    res.send('API is working');
+});
+
 const PORT = process.env.PORT || 4999;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
